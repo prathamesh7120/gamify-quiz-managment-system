0ba11f9ecc37fdd157d8344033e53ab2ce40120e
Laukik palav <palavlaukik99@email.com>
2026-04-08 23:17:51 +0530

    WelcomeCard created

---
+++ frontend/src/components/WelcomeCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Welcome</h1>
+
+      <p>Welcome to our website!</p>
+      <p> Built by Laukik</p>
+    </div>
+  );
+}
+
+export default App;
